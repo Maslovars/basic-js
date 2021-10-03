c085f19f57dad00fc79f10bb3677e07d0089d823
Maslovars <maslovars@gmail.com>
2021-10-04 01:00:53 +0300

    feat: add tasks

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -13,8 +13,19 @@ import { NotImplementedError } from '../extensions/index.js';
  *
  */
 export default class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  calculateDepth(arr) {
+    let depth = 1;
+    if (arr.find(i => Array.isArray(i)) === undefined) {
+      return depth;
+    } else {
+      let count = [];
+      for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr[i].length; j++) {
+          count.push(arr[i][j])
+        }
+      }
+      depth += this.calculateDepth(count);
+      return depth;
+    }
   }
 }
