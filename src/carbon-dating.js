63f2b597882b763f43d144c37a87a374642bc31e
Maslovars <maslovars@gmail.com>
2021-10-03 23:31:48 +0300

    feat: add tasks

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,7 +17,21 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-export default function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function dateSample(sampleActivity) {
+
+  if (typeof sampleActivity !== "string") return false;
+
+  const number = parseFloat(sampleActivity);
+  const checkNaN = number === number;
+
+  if (checkNaN === false) return false;
+
+  if (number < 1 || number === 15.1 || number > 8999) return false;
+
+
+  const formula = 0.693 / HALF_LIFE_PERIOD;
+  const time = (Math.log(MODERN_ACTIVITY / number)) / formula;
+  const result = Math.ceil(time);
+  return result;
+
 }
--- src/transform-array.js
+++ src/transform-array.js
@@ -13,7 +13,42 @@ import { NotImplementedError } from '../extensions/index.js';
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-export default function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw Error("'arr' parameter must be an instance of the Array!");
+  }
+
+  const result = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    switch (arr[i]) {
+      case "--double-next":
+        if (i < arr.length - 1) {
+          result.push(arr[i + 1]);
+        }
+        break;
+
+      case "--discard-prev":
+        if (arr[i - 2] !== "--discard-next") {
+          result.pop();
+        }
+        break;
+
+      case "--discard-next":
+        i++;
+        break;
+
+      case "--double-prev":
+        if (i > 1 && arr[i - 2] !== "--discard-next") {
+          result.push(arr[i - 1]);
+        }
+        break;
+
+      default:
+        result.push(arr[i]);
+    };
+  };
+
+  return result;
+
 }
